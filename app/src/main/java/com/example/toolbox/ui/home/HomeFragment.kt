63422c9c6d693c48e3ed1b81d5f429d25a91eda2
Rick.Derthick <rcderthick@gmail.com>
2024-03-20 19:35:37 -0400

    Color scheme set

--- app/src/main/java/com/example/toolbox/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/toolbox/ui/home/HomeFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.toolbox.databinding.FragmentHomeBinding
+import com.example.toolbox.databinding.FragmentEngineeringBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentEngineeringBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentEngineeringBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
--- app/src/main/java/com/example/toolbox/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/toolbox/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is the Engineering Fragment"
     }
     val text: LiveData<String> = _text
 }
