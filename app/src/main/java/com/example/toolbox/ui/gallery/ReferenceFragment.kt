da946ef7766f62791d97a05128799298e0a99964
Rick.Derthick <rcderthick@gmail.com>
2024-03-28 16:10:07 -0400

    Gallery renamed to reference

--- app/src/main/java/com/example/toolbox/ui/gallery/ReferenceFragment.kt
+++ app/src/main/java/com/example/toolbox/ui/gallery/ReferenceFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.toolbox.databinding.FragmentGalleryBinding
+import com.example.toolbox.databinding.FragmentReferenceBinding
 
-class GalleryFragment : Fragment() {
+class ReferenceFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentReferenceBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val referenceViewModel =
+            ViewModelProvider(this).get(ReferenceViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentReferenceBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        referenceViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/toolbox/ui/gallery/ReferenceViewModel.kt
+++ app/src/main/java/com/example/toolbox/ui/gallery/ReferenceViewModel.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class ReferenceViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is reference Fragment"
     }
     val text: LiveData<String> = _text
 }
